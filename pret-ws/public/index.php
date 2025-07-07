24e10c825e9746036f966a4fc188fc2be3352782
Shania <ralijaonashania@gmail.com>
2025-07-07 18:19:29 +0300

    front se connecter

--- pret-ws/public/index.php
+++ pret-ws/public/index.php
@@ -2,16 +2,23 @@
 require '../vendor/autoload.php';
 
 // Route pour afficher le formulaire
-Flight::route('GET /', function() {
-   include __DIR__ . '/../app/views/auth/login.html';
+Flight::route('GET /signup', function() {
+   include __DIR__ . '/../app/views/auth/signUp.html';
 });
 
+// Route pour afficher la page de connexion
+Flight::route('GET /', function() {
+   include __DIR__ . '/../app/views/auth/signIn.html';
+});
+
+// Redirige la racine vers /signup
+Flight::route('GET /', function() {
+    Flight::redirect('/signup');
+});
 
 Flight::route('POST /api/auth/register', function() {
     header('Content-Type: application/json');
     $data = json_decode(file_get_contents('php://input'), true);
-    
-
     if (empty($data['fullName'])) {
         Flight::json(['errors' => ['fullName' => 'Nom requis']], 400);
     } else {
